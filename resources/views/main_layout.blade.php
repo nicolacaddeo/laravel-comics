e48b106011db5781ba3d79d5317fda83be4b4dc8
nicolacaddeo <nicola.caddeo58@gmail.com>
2022-11-01 17:58:56 +0100

    completed header layout

--- resources/views/main_layout.blade.php
+++ resources/views/main_layout.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Homepage</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Document</title>
 </head>
 
 <body>
-    <h1>Hello World!</h1>
+    @include('partials.header')
 </body>
 
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,29 @@
+<div class="blue-header"></div>
+
+<nav>
+    <div class="nav-container">
+        <div id="logo-container">
+            <img src="{{ asset('images/dc-logo.png') }}" alt="DC logo">
+        </div>
+        <div id="links-container">
+            <ul>
+                <li>characters</li>
+                <li>comics</li>
+                <li>movies</li>
+                <li>tv</li>
+                <li>games</li>
+                <li>collectibles</li>
+                <li>videos</li>
+                <li>fans</li>
+                <li>news</li>
+                <li>shop</li>
+            </ul>
+        </div>
+        <div id="search-bar">
+            <input type="text" placeholder="Search">
+        </div>
+    </div>
+</nav>
+<div id="jumbotron">
+
+</div>
